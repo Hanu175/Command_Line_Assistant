14e4dee485c5686832fb75335f81855d97159cd0
Vedant Paste <pastevedu@gmail.com>
2026-09-01 23:11:14 +0530

    Completion of Milestone 5

--- core/parser.py
+++ core/parser.py
@@ -32,12 +32,16 @@ class CommandContext:
             f")"
         )
 
-
 def parse_command_line(command_line):
     """
     Parse a command line and determine what the user
     is currently trying to complete.
     """
+
+    ends_with_space = (
+        command_line.endswith(" ")
+        or command_line.endswith("\t")
+    )
 
     try:
         tokens = shlex.split(command_line)
@@ -46,6 +50,7 @@ def parse_command_line(command_line):
         # fall back to basic splitting.
         tokens = command_line.split()
 
+    # Empty command line
     if not tokens:
         return CommandContext(
             raw_input=command_line,
@@ -57,17 +62,30 @@ def parse_command_line(command_line):
         )
 
     command = tokens[0]
-    current_token = tokens[-1]
-    position = len(tokens) - 1
 
-    if len(tokens) == 1:
-        context_type = "command"
+    # Case: user finished the previous token and
+    # is starting a new one.
+    if ends_with_space:
+        current_token = ""
+        position = len(tokens)
 
-    elif current_token.startswith("-"):
-        context_type = "option"
+        if len(tokens) == 1:
+            context_type = "subcommand_or_option"
+        else:
+            context_type = "argument_or_subcommand"
 
     else:
-        context_type = "subcommand_or_argument"
+        current_token = tokens[-1]
+        position = len(tokens) - 1
+
+        if len(tokens) == 1:
+            context_type = "command"
+
+        elif current_token.startswith("-"):
+            context_type = "option"
+
+        else:
+            context_type = "subcommand_or_argument"
 
     return CommandContext(
         raw_input=command_line,
